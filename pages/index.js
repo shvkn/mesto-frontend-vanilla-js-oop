eb48ca4d007e5db4deb81ffce89be9788d03699a
shvkn <a.shuvakhin@gmail.com>
2022-06-23 19:11:29 +0700

    feat (places): Add JS into places block. Render from Array of Objects. Construct Place block from template

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,44 @@ const data = {
     caption: 'Исследователь океана',
     avatar: 'images/profile-avatar.jpg',
   },
+
+  places: [
+    {
+      heading: 'Гора Эльбрус',
+      img: { src: './images/elements-elbrus.jpg', alt: 'Гора Эльбрус на закате.' },
+      liked: true,
+    },
+    {
+      heading: 'Домбай',
+      img: { src: './images/elements-dombay.jpg', alt: 'Гора Пик Инэ, Домбай.' },
+      liked: false,
+    },
+    {
+      heading: 'Карачаево-Черкессия',
+      img: { src: './images/elements-karachay.jpg', alt: 'Древний храм в Карачаево-Черкессии.' },
+      liked: true,
+    },
+    {
+      heading: 'Алтай',
+      img: { src: './images/elements-altay.jpg', alt: 'Бирбзовая Катунь, Алтай.' },
+      liked: false,
+    },
+    {
+      heading: 'Бурятия',
+      img: { src: './images/elements-buratia.jpg', alt: 'Буддистский дацан, Бурятия.' },
+      liked: false,
+    },
+    {
+      heading: 'Сочи',
+      img: { src: './images/elements-sochi.jpg', alt: 'Пирс, Сочи.' },
+      liked: false,
+    },
+  ],
 };
 
 const editProfilePopup = document.querySelector('#edit-profile-popup');
 const profile = document.querySelector('.profile');
+const placesContainer = document.querySelector('.places');
 
 const renderProfile = () => {
   profile.querySelector('.profile__name').textContent = data.profile.name;
@@ -50,4 +84,29 @@ editProfilePopup.querySelector('.popup__submit-btn').addEventListener('click', (
   closePopup(e);
 });
 
+const addNewPlace = (imageLink, imageDesc, heading, liked) => {
+  const placeTemplate = document.querySelector('#place-template').content;
+  const place = placeTemplate.querySelector('.place').cloneNode(true);
+
+  const likeButton = place.querySelector('.place__like-btn');
+
+  place.querySelector('.place__image').src = imageLink;
+  place.querySelector('.place__image').alt = imageDesc;
+  place.querySelector('.place__heading').textContent = heading;
+
+  if (liked) {
+    likeButton.classList.add('place__like-btn_active');
+  }
+
+  placesContainer.append(place);
+};
+
+const renderPlaces = () => data.places.forEach((placeObj) => addNewPlace(
+  placeObj.img.src,
+  placeObj.img.desc,
+  placeObj.heading,
+  placeObj.liked,
+));
+
 renderProfile();
+renderPlaces();
