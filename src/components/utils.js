34655fd46bd67adee596a0e8af284836ad19df93
Anatoly Shuvakhin <a.shuvakhin@gmail.com>
2022-08-26 14:17:26 +0700

    fix: Рефакторинг функций работы с профайлом

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,36 +1,19 @@
 import {
   cardsContainerEl,
-  profileAvatarEl,
-  profileCaptionEl,
-  profileFormCaptionEl,
-  profileFormNameEl,
-  profileNameEl,
 } from './index';
+
 import { data } from './data';
 import { createCardNode } from './card';
 
-export const renderProfile = () => {
-  profileNameEl.textContent = data.profile.name;
-  profileCaptionEl.textContent = data.profile.caption;
-  profileAvatarEl.src = data.profile.avatar;
-  profileFormNameEl.value = data.profile.name;
-  profileFormCaptionEl.value = data.profile.caption;
-};
+const profileEl = document.querySelector('.profile');
+const profileNameEl = profileEl.querySelector('.profile__name');
+const profileCaptionEl = profileEl.querySelector('.profile__caption');
 
-export const setProfileData = (name, caption, avatar) => {
-  data.profile.name = name;
-  data.profile.caption = caption;
-  data.profile.avatar = avatar;
-  renderProfile();
-};
+
 
 export const addCardToContainer = (card) => {
   cardsContainerEl.prepend(card);
 };
 
-export const renderCards = () => {
-  data.places.forEach((cardObj) => {
-    const cardNode = createCardNode(cardObj.name, cardObj.link);
-    addCardToContainer(cardNode);
-  });
-};
+
+
