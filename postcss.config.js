e368bce70fafe28e111d51a5d8e3e00d764870e9
Anatoly Shuvakhin <a.shuvakhin@gmail.com>
2022-09-04 15:14:42 +0700

    Updates

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-const autoprefixier = require('autoprefixer');
-const cssnano = require('cssnano');
-
-module.exports = {
-  plugins: [
-    autoprefixier,
-    cssnano({ preset: 'default' }),
-  ],
-};
